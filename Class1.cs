2afc4b69baee9b6c7b90188ae3f5c7e160cee6ad
shabahat97 <umernasir19@gmail.com>
2021-01-25 18:46:37 +0500

    first commit

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace GeneticAlgorithm
+{
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Population TestPopulation = new Population();
+			TestPopulation.WriteNextGeneration();
+
+			for (int i = 0; i < 1000; i++)
+			{
+				TestPopulation.NextGeneration();
+				TestPopulation.WriteNextGeneration();
+			}
+
+			Console.ReadLine();
+			//
+			// TODO: Add code to start application here
+			//
+		}
+	}
+}
